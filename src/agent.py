8d4ca5bd0c7624d5037fe8a741e78e0a219481d7
vineetsarpal <vineetsarpal02@gmail.com>
2025-07-08 16:39:49 -0400

    Add uv

--- src/agent.py
+++ src/agent.py
@@ -24,7 +24,7 @@ tools = [tavily_search_tool, add, subtract, multiply, divide]
 # State
 class State(MessagesState):
     # Add additional keys beyond the built in messages
-    summary: str
+    pass
 
 # Following is what the built in Langgraph MessagesState is
 # class MessagesState(TypedDict):
@@ -39,81 +39,27 @@ sys_msg = SystemMessage(content="You are a helpful assistant!")
 
 # Node
 def assistant(state: State):
-    # Get summary if exists
-    summary = state.get("summary", "")
-    if summary:
-        sys_msg_summary = f"Summary of conversation earlier: {summary}"
-        messages = [SystemMessage(content=sys_msg_summary)] + state["messages"]
-    else:
-        messages = state["messages"]
-    response = llm_with_tools.invoke(messages)
-    return {"messages": response}
+      return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
 
-
-def summarize_conversation(state: State):
-    summary = state.get("summary", "")
-
-    # Create a summarization prompt
-    if summary:
-        summary_msg = (
-            f"This is the summary of the conversation till now: {summary}\n\n"
-            "Extend the summary by taking into account the new messages above"
-        )
-    else:
-        summary_msg = "Create a summary of the conversation above"
-
-    messages = state["messages"] + [HumanMessage(content=summary_msg)]
-    response = llm_with_tools.invoke(messages)
-
-    # Delete all but the 2 most recent messages
-    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
-    return {"summary": response.content, "messages": delete_messages}
-
-
-def tools_summarize_condition(state: State) -> Literal["tools", "summarize_conversation", "__end__"]:
-    """ Return the next node to execute""" 
-    messages = state["messages"]
-    last_message = messages[-1]
-
-    if last_message.tool_calls:
-        return "tools"
-    
-    human_msg_count = sum(1 for m in messages if isinstance(m, HumanMessage))
-    if human_msg_count > 3:
-        return "summarize_conversation"
-    
-    return END
 
 # Build graph
 graph_builder = StateGraph(State)
 graph_builder.add_node("assistant", assistant)
 graph_builder.add_node("tools", ToolNode(tools))
-graph_builder.add_node("summarize_conversation", summarize_conversation)
 
 graph_builder.add_edge(START, "assistant")
-graph_builder.add_conditional_edges("assistant", tools_summarize_condition)
+graph_builder.add_conditional_edges("assistant", tools_condition)
 graph_builder.add_edge("tools", "assistant")
-graph_builder.add_edge("summarize_conversation", END)
 
+# # Compile Graph Witout Memory
+# graph = graph_builder.compile()
 
-# # Build graph
-# graph_builder = StateGraph(State)
-# graph_builder.add_node("assistant", assistant)
-# graph_builder.add_node("tools", ToolNode(tools))
-
-# graph_builder.add_edge(START, "assistant")
-# graph_builder.add_conditional_edges("assistant", tools_condition)
-# graph_builder.add_edge("tools", "assistant")
-
-# Compile Graph Witout Memory
-graph = graph_builder.compile()
-
-# # Compile Graph with memory
-# memory = MemorySaver()
-# graph = graph_builder.compile(checkpointer=memory)
+# Compile Graph with memory
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
 
 # Specify a thread
-config = {"configurable": {"thread_id": "1"}}
+# config = {"configurable": {"thread_id": "1"}}
 
 # # Input
 # messages = [HumanMessage("Add 3 and 4")]
